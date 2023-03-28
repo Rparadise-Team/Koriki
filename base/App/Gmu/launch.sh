4a515d16c68a2371cdd79b4f593351f0d1d9b67d
Ninoh-FOX <ninohfox@gmail.com>
2023-03-28 16:02:38 +0200

    fix audioserver for models

--- base/App/Gmu/launch.sh
+++ base/App/Gmu/launch.sh
@@ -9,7 +9,7 @@ cd $my_dir
 
 runsvr=`/customer/app/jsonval audiofix`
 if [ "$runsvr" != "0" ] ; then
-    LD_PRELOAD=/mnt/SDCARD/Koriki/lib/as_preload.so /mnt/SDCARD/Koriki/bin/audioserver.mod &
+    LD_PRELOAD=/mnt/SDCARD/Koriki/lib/as_preload.so /mnt/SDCARD/Koriki/bin/audioserver > /dev/null 2>&1 &
     if [ -f /customer/lib/libpadsp.so ]; then
         export LD_PRELOAD=/customer/lib/libpadsp.so
     fi
