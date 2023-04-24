6731e0a6201fce2fb278f9a4445a1bc79a94b976
Ninoh-FOX <ninohfox@gmail.com>
2023-04-25 00:43:35 +0200

    Update main.c

--- src/OverlaySelector/main.c
+++ src/OverlaySelector/main.c
@@ -1,167 +1,167 @@
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
+#include <SDL/SDL_gfxPrimitives.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <stdbool.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <string.h>
 #include <linux/fb.h>
 #include <linux/input.h>
+#include <stdbool.h>
 #include <sys/stat.h>
-#include "sys/ioctl.h"
+#include <sys/ioctl.h>
 #include <dirent.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 #define SCREEN_BPP 32
 
-#define	BUTTON_A	SDLK_SPACE
-#define	BUTTON_B	SDLK_LCTRL
-#define	BUTTON_X	SDLK_LSHIFT
-#define	BUTTON_Y	SDLK_LALT
-#define	BUTTON_START	SDLK_RETURN
-#define	BUTTON_SELECT	SDLK_RCTRL
-#define	BUTTON_MENU	SDLK_ESCAPE
-#define	BUTTON_L2	SDLK_TAB
-#define	BUTTON_R2	SDLK_BACKSPACE
+#define BUTTON_A SDLK_SPACE
+#define BUTTON_B SDLK_LCTRL
+#define BUTTON_X SDLK_LSHIFT
+#define BUTTON_Y SDLK_LALT
+#define BUTTON_START SDLK_RETURN
+#define BUTTON_SELECT SDLK_RCTRL
+#define BUTTON_MENU SDLK_ESCAPE
+#define BUTTON_L2 SDLK_TAB
+#define BUTTON_R2 SDLK_BACKSPACE
 #define BUTTON_RIGHT SDLK_RIGHT
 #define BUTTON_LEFT SDLK_LEFT
 
-#define CONSOLA "GB" //a cambiar por consola
-#define CORE "Gambatte" //a cambiar por emulador
+#define CONSOLA "GB"
+#define CORE "Gambatte"
+
+#define NUM_IMAGES 3
+
 #define TEXTO1 "video_dingux_ipu_keep_aspect"
 #define TEXTO2 "video_scale_integer"
 #define TEXTO3 "custom_viewport_height"
 #define TEXTO4 "input_overlay"
 
-#define VALOR1 "False"
-#define VALOR2 "True"
-#define VALOR3 ":/.retroarch/overlay/ATC/ATC-GB.cfg" //a cambiar por consola
-#define VALOR4 "" // sin overlay
+#define VALOR1 "false"
+#define VALOR2 "true"
+#define VALOR3 ":/.retroarch/overlay/ATC/ATC-GB.cfg"
+#define VALOR4 ""
+#define VALOR5 "576"
 
 SDL_Surface* screen = NULL;
-SDL_Surface* overlayImage = NULL;
-char img_name[256] = "1.png";
-const char* text_values[4] = {TEXTO1, TEXTO2, TEXTO3, TEXTO4};
-const char* text_bools[4] = {VALOR1, VALOR2, VALOR3, VALOR4};
-const char* text_selected_values[3][4] = {
-    {VALOR2, VALOR1, "576", VALOR4},// aspect ration
-    {VALOR2, VALOR2, "576", VALOR3},// overlay
-    {VALOR1, VALOR1, "576", VALOR4}// fullscreen
-};
+SDL_Surface* image[NUM_IMAGES];
+SDL_Surface* text_surface = NULL;
+TTF_Font* font = NULL;
+SDL_Rect image_rect;
+SDL_Event event;
+bool running = true;
+int current_image = 0;
 
-void update_overlay(char* img_name) {
-    char opt_path[256] = "/mnt/SDCARD/RetroArch/.retroarch/config/" CORE "/" CONSOLA ".cfg";
-    char opt_new_path[256] = CONSOLA".cfg.new";
-    FILE *opt_file, *opt_new_file;
-    opt_file = fopen(opt_path, "r");
-    opt_new_file = fopen(opt_new_path, "w");
-
-    if (opt_file == NULL || opt_new_file == NULL) {
-        printf("Error al abrir el archivo de configuracion\n");
-        return;
-    }
-
-    char line[256];
-    while (fgets(line, sizeof(line), opt_file)) {
-        int i;
-        for (i = 0; i < 5; i++) {
-            const char* text_value = text_selected_values[atoi(img_name)-1][i];
-            if (strstr(line, text_values[i]) != NULL) {
-                fprintf(opt_new_file, "%s=%s\n", text_values[i], text_value);
-                break;
-            }
-        }
-        if (i == 5) {
-            fprintf(opt_new_file, "%s", line);
-        }
-    }
-    fclose(opt_file);
-    fclose(opt_new_file);
-    remove(opt_path);
-    rename(opt_new_path, opt_path);
+void load_image(int index) {
+    char filename[16];
+    sprintf(filename, "%d.png", index + 1);
+    image[index] = IMG_Load(filename);
 }
-						   
-int main(int argc, char* args[]) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) == -1) {
-        printf("Error initializing SDL.\n");
-        return 1;
-    }
 
+void update_config(const char* filename, const char* texto1, const char* valor1, const char* texto2, const char* valor2, const char* texto3, const char* valor3, const char* texto4, const char* valor4) {
+    char buffer[100];
+    sprintf(buffer, "/mnt/SDCARD/RetroArch/.retroarch/config/%s/%s.cfg", CORE, CONSOLA);
+    if (access(buffer, F_OK) != -1) {
+        remove(buffer);
+    }
+    FILE* file = fopen(buffer, "w");
+    if (file != NULL) {
+		if (strlen(texto1) > 0 && strlen(valor1) > 0) {
+        fprintf(file, "%s = \"%s\"\n", texto1, valor1);
+		}
+		if (strlen(texto2) > 0 && strlen(valor2) > 0) {
+        fprintf(file, "%s = \"%s\"\n", texto2, valor2);
+		}
+        if (strlen(texto3) > 0 && strlen(valor3) > 0) {
+            fprintf(file, "%s = \"%s\"\n", texto3, valor3);
+        }
+        if (strlen(texto4) > 0 && strlen(valor4) > 0) {
+            fprintf(file, "%s = \"%s\"\n", texto4, valor4);
+        }
+        fclose(file);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) == -1) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return -1;
+    }
     screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
     if (screen == NULL) {
-        printf("Error setting SDL video mode.\n");
-        return 1;
+        printf("Error setting video mode: %s\n", SDL_GetError());
+        return -1;
+    }
+    for (int i = 0; i < NUM_IMAGES; i++) {
+        load_image(i);
     }
 
-    if (IMG_Init(IMG_INIT_PNG) == -1) {
-        printf("Error initializing SDL_image.\n");
-        return 1;
-    }
-
-    int quit = 0;
-    SDL_Event event;
-    int selected_image_index = 0;
-
-    while (!quit) {
+    image_rect.x = (SCREEN_WIDTH - image[current_image]->w) / 2;
+    image_rect.y = (SCREEN_HEIGHT - image[current_image]->h) / 2;
+    image_rect.w = image[current_image]->w;
+    image_rect.h = image[current_image]->h;
+	
+	char FILECONFIG[100];
+    sprintf(FILECONFIG, "/mnt/SDCARD/RetroArch/.retroarch/config/%s/%s.cfg", CORE, CONSOLA);
+	
+    while (running) {
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
-                    quit = 1;
+                    running = false;
                     break;
                 case SDL_KEYDOWN:
-                    if (event.key.keysym.sym == SDLK_LEFT) {
-                        selected_image_index--;
-                        if (selected_image_index < 0) {
-                            selected_image_index = 2;
-                        }
-                        char new_img_name[16];
-                        sprintf(new_img_name, "%d.png", selected_image_index+1);
-                        update_overlay(new_img_name);
-                        strcpy(img_name, "./");
-						strcat(img_name, new_img_name);
-                    } else if (event.key.keysym.sym == SDLK_RIGHT) {
-                        selected_image_index++;
-                        if (selected_image_index > 2) {
-                            selected_image_index = 0;
-                        }
-                        char new_img_name[16];
-                        sprintf(new_img_name, "%d.png", selected_image_index+1);
-                        update_overlay(new_img_name);
-                        strcpy(img_name, "./");
-                        strcat(img_name, new_img_name);
-                    } else if (event.key.keysym.sym == SDLK_SPACE) {
-                        char new_img_name[16];
-                        sprintf(new_img_name, "%d.png", selected_image_index+1);
-                        update_overlay(new_img_name);
-                    } else if (event.key.keysym.sym == SDLK_ESCAPE) {
-						quit = 1;
-					} else if (event.key.keysym.sym == SDLK_LCTRL) {
-						quit = 1;
-					}
+                    if (event.key.keysym.sym == BUTTON_RIGHT) {
+                            current_image = (current_image + 1) % NUM_IMAGES;
+                            image_rect.x = (SCREEN_WIDTH - image[current_image]->w) / 2;
+                            image_rect.y = (SCREEN_HEIGHT - image[current_image]->h) / 2;
+                            break;
+					} else if (event.key.keysym.sym == BUTTON_LEFT) {
+                            current_image = (current_image - 1 + NUM_IMAGES) % NUM_IMAGES;
+                            image_rect.x = (SCREEN_WIDTH - image[current_image]->w) / 2;
+                            image_rect.y = (SCREEN_HEIGHT - image[current_image]->h) / 2;
+                            break;
+					} else if (event.key.keysym.sym == BUTTON_A) {
+                            switch (current_image) {
+                                case 0:
+                                    update_config(FILECONFIG, TEXTO1, VALOR2, TEXTO2, VALOR1, "", "", "", ""); //aspect ratio
+									boxColor(screen, 0, 0, 639, 479, 0x00000000);
+									stringColor(screen, 320, 240, "Done! Press B to exit!", 0xFFFFFFFF);
+									SDL_Flip(screen);
+									SDL_Delay(3000);
+                                    break;
+                                case 1:
+                                    update_config(FILECONFIG, TEXTO1, VALOR2, TEXTO2, VALOR2, TEXTO3, VALOR5, TEXTO4, VALOR3); //overlay
+									boxColor(screen, 0, 0, 639, 479, 0x00000000);
+									stringColor(screen, 320, 240, "Done! Press B to exit!", 0xFFFFFFFF);
+									SDL_Flip(screen);
+									SDL_Delay(3000);
+                                    break;
+                                case 2:
+                                    update_config(FILECONFIG, TEXTO1, VALOR1, TEXTO2, VALOR1, "", "", "", ""); //fullscreen
+									boxColor(screen, 0, 0, 639, 479, 0x00000000);
+									stringColor(screen, 320, 240, "Done! Press B to exit!", 0xFFFFFFFF);
+									SDL_Flip(screen);
+									SDL_Delay(3000);
+                                    break;
+                            }
+                            break;
+					} else if (event.key.keysym.sym == BUTTON_B || event.key.keysym.sym == BUTTON_MENU) {
+                            running = false;
+                            break;
+                    }
                     break;
             }
         }
-
-        overlayImage = IMG_Load(img_name);
-        if (overlayImage == NULL) {
-            printf("Error loading overlay image.\n");
-            return 1;
-        }
-
-        SDL_Rect overlayRect;
-        overlayRect.x = 0;
-        overlayRect.y = 0;
-        SDL_BlitSurface(overlayImage, NULL, screen, &overlayRect);
-        SDL_FreeSurface(overlayImage);
-
-        SDL_Flip(screen);
+		SDL_FillRect(screen, NULL, SDL_MapRGB(screen->format, 0, 0, 0));
+		SDL_BlitSurface(image[current_image], NULL, screen, &image_rect);
+		SDL_Flip(screen);
     }
-
+	TTF_CloseFont(font);
+	TTF_Quit();
     SDL_Quit();
-
     return 0;
 }
