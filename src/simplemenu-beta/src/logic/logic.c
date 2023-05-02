27716cb8f7a24dd9f107fdf81bd8a045e979fba5
Ninoh-FOX <ninohfox@gmail.com>
2023-05-02 16:10:32 +0200

    fixed menu shutdown without audiofix

--- src/simplemenu-beta/src/logic/logic.c
+++ src/simplemenu-beta/src/logic/logic.c
@@ -206,10 +206,19 @@ void quit() {
 			execlp("sh", "sh", "-c", "sync && reboot", NULL);
 		} else {
 			#ifdef MIYOOMINI
-			if (mmModel)
-				execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.min && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
-			else
-				execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.plu && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			audioFix = getCurrentSystemValue("audiofix");
+			
+			if (audioFix == 1){
+				if (mmModel)
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.min && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
+				else
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.plu && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			} else if (audioFix == 0){
+				if (mmModel)
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
+				else
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			}	
 			#else
 			execlp("sh", "sh", "-c", "sync && poweroff", NULL);
 			#endif
@@ -219,10 +228,19 @@ void quit() {
 			execlp("sh", "sh", "-c", "sync && reboot", NULL);
 		} else if (selectedShutDownOption == 2) {
 			#ifdef MIYOOMINI
-			if (mmModel)
-				execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.min && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
-			else
-				execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.plu && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			audioFix = getCurrentSystemValue("audiofix");
+			
+			if (audioFix == 1){
+				if (mmModel)
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.min && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
+				else
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall audioserver && killall audioserver.plu && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			} else if (audioFix == 0){
+				if (mmModel)
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && reboot", NULL);
+				else
+					execlp("sh", "sh", "-c", "killall main && killall updater && killall keymon && swapoff /mnt/SDCARD/cachefile && sync && sleep 5 && poweroff", NULL);
+			}
 			#else
 			execlp("sh", "sh", "-c", "sync && poweroff", NULL);
 			#endif
