d9ae4c0fcc0469e2791c56395f408443d8aaf960
Ninoh-FOX <ninoh-fox@gmail.com>
2023-05-06 20:11:37 +0200

    update GMU script

--- beta/App/Gmu/launch.sh
+++ beta/App/Gmu/launch.sh
@@ -4,11 +4,16 @@ my_dir=`dirname $0`
 runsvr=`/customer/app/jsonval audiofix`
 
 if [ "$runsvr" != "0" ] ; then
-    FILE=/customer/app/axp_test
+	FILE=/customer/app/axp_test
 
     if [ -f "$FILE" ]; then
         killall audioserver
 		killall audioserver.plu
+		FILE2=/tmp/audioserver_on
+		if [ -f "$FILE2" ]; then
+			rm /tmp/audioserver_on
+			/customer/app/sysmon freemma
+		fi
     else
         killall audioserver
 		killall audioserver.min
@@ -20,8 +25,13 @@ cd $my_dir
 
 runsvr=`/customer/app/jsonval audiofix`
 if [ "$runsvr" != "0" ] ; then
+	FILE=/customer/app/axp_test
+	if [ -f "$FILE" ]; then
+	touch /tmp/audioserver_on
+	fi
 	/mnt/SDCARD/Koriki/bin/audioserver &
-    if [ -f /customer/lib/libpadsp.so ]; then
+	if [ -f /customer/lib/libpadsp.so ]; then
 		export LD_PRELOAD=/customer/lib/libpadsp.so
 	fi
 fi
+
