87c7a9f62848707cd007063b9ad67b286bbc9869
Ninoh-FOX <ninohfox@gmail.com>
2023-08-09 16:36:11 +0200

    Reverse launch.sh from GMU

--- base/App/Gmu/launch.sh
+++ base/App/Gmu/launch.sh
@@ -1,16 +1,37 @@
 #!/bin/sh
 
 my_dir=`dirname $0`
+runsvr=`/customer/app/jsonval audiofix`
 
-killall audioserver.mod
+if [ "$runsvr" != "0" ] ; then
+	FILE=/customer/app/axp_test
+
+    if [ -f "$FILE" ]; then
+        killall audioserver
+		killall audioserver.plu
+		FILE2=/tmp/audioserver_on
+		if [ -f "$FILE2" ]; then
+			rm /tmp/audioserver_on
+			/customer/app/sysmon freemma
+		fi
+    else
+        killall audioserver
+		killall audioserver.min
+    fi
+fi
 
 cd $my_dir
 ./launch2.sh
 
 runsvr=`/customer/app/jsonval audiofix`
 if [ "$runsvr" != "0" ] ; then
-    LD_PRELOAD=/mnt/SDCARD/Koriki/lib/as_preload.so /mnt/SDCARD/Koriki/bin/audioserver.mod &
-    if [ -f /customer/lib/libpadsp.so ]; then
-        export LD_PRELOAD=/customer/lib/libpadsp.so
-    fi
+	FILE=/customer/app/axp_test
+	if [ -f "$FILE" ]; then
+	touch /tmp/audioserver_on
+	fi
+	/mnt/SDCARD/Koriki/bin/audioserver &
+	if [ -f /customer/lib/libpadsp.so ]; then
+		export LD_PRELOAD=/customer/lib/libpadsp.so
+	fi
 fi
+
