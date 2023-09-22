0676d07aa2f26619b8ee04edd8e4c0cb55aaf8a2
Ninoh-FOX <ninohfox@gmail.com>
2023-09-22 13:48:15 +0200

    added music selector for simplemenu

---
+++ base/App/Musicselector/Musicselector.py
@@ -0,0 +1,85 @@
+import pygame
+import os
+from pygame.locals import *
+
+MUSIC_PATH = "/mnt/SDCARD/App/Musicselector/music/"
+MUSIC_WAV_PATH = "/mnt/SDCARD/Media/music.wav"
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Music Selector")
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+font = pygame.font.Font(None, 36)
+
+songs = os.listdir(MUSIC_PATH)
+songs.insert(0, "NO MUSIC")
+
+selected_song = None
+
+def list_songs():
+    global selected_song
+    clock = pygame.time.Clock()
+    selected_index = 0
+
+    while True:
+        screen.fill(BLACK)
+        
+        for i, song in enumerate(songs):
+            text = font.render(song, True, RED)
+            rect = text.get_rect(topleft=(60, 100 + i * 50))
+            screen.blit(text, rect)
+
+            if i == selected_index:
+                pygame.draw.circle(screen, WHITE, (50, 100 + i * 50), 10)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    selected_index = max(0, selected_index - 1)
+                elif event.key == K_DOWN:
+                    selected_index = min(len(songs) - 1, selected_index + 1)
+                elif event.key == K_SPACE:
+                    selected_song = songs[selected_index]
+                    return
+                elif event.key == K_ESCAPE:
+                    pygame.quit()
+                    return
+
+def delete_music():
+    os.remove(MUSIC_WAV_PATH)
+
+def convert_to_wav(song_name):
+    command = '/mnt/SDCARD/Koriki/bin/ffmpeg -y -i "{}{}" "{}"'.format(MUSIC_PATH, song_name, MUSIC_WAV_PATH)
+    os.system(command)
+
+def display_message(message):
+    font = pygame.font.Font(None, 36)
+    text = font.render(message, True, RED)
+    rect = text.get_rect(center=(320, 240))
+    screen.blit(text, rect)
+    pygame.display.flip()
+
+def wait_for_conversion():
+    display_message("Please wait, converting file...")
+	
+if __name__ == "__main__":
+    list_songs()
+
+    if selected_song == "NO MUSIC":
+        delete_music()
+        print "file music.wav erased."
+    elif selected_song:
+        wait_for_conversion()
+        convert_to_wav(selected_song)
+        print "file convert and copy to music.wav."
---
+++ base/App/Musicselector/launch.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/mnt/SDCARD/Koriki/bin/python /mnt/SDCARD/App/Musicselector/Musicselector.py
