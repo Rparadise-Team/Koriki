5f433a00f5fcef506afa1af26af121a90849baac
Ninoh-FOX <ninohfox@gmail.com>
2024-02-21 19:31:12 +0100

    added ssh server

--- base/App/ssh/ssh.py
+++ base/App/ssh/ssh.py
@@ -12,7 +12,7 @@ SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
 start_color = (0, 0, 0)
-end_color = (0, 100, 0)
+end_color = (100, 0, 0)
 duration = 5 * 1000
 steps = 100
 
@@ -52,8 +52,8 @@ def get_ip_address():
 def display_data(screen, font):
     center_x = SCREEN_WIDTH // 2
 
-    # Display "Koriki FTP server" at the top of the screen
-    title_text = font.render("Koriki FTP server", True, (255, 255, 255))
+    # Display "Koriki SSH server" at the top of the screen
+    title_text = font.render("Koriki SSH server", True, (255, 255, 255))
     title_text_rect = title_text.get_rect(center=(center_x, 30))
     screen.blit(title_text, title_text_rect)
 
@@ -64,32 +64,44 @@ def display_data(screen, font):
         warning_text_rect = warning_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2))
         screen.blit(warning_text, warning_text_rect)
     else:
-        ip_text = font.render("Set IP: " + ip_address, True, (255, 255, 255))
-        port_text = font.render("user ftp and port 21 in FileZilla", True, (255, 255, 255))
-        ip_text_rect = ip_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2))
-        port_text_rect = port_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2 + 50))
+        ip_text = font.render('type "ssh koriki@' + ip_address + '" in the terminal', True, (255, 255, 255))
+        ip_password = font.render('password "koriki"', True, (255, 255, 255))
+        or_text = font.render('or', True, (255, 255, 255))
+        root_text = font.render('type "ssh root@' + ip_address + '" in the terminal', True, (255, 255, 255))
+        root_password = font.render('not password', True, (255, 255, 255))
+        ip_text_rect = ip_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2 - 50))
+        ip_password_rect = ip_password.get_rect(center=(center_x, SCREEN_HEIGHT // 2 - 25))
+        or_text_rect = or_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2))
+        root_text_rect = root_text.get_rect(center=(center_x, SCREEN_HEIGHT // 2 + 25))
+        root_password_rect = root_password.get_rect(center=(center_x, SCREEN_HEIGHT // 2 + 50))
         screen.blit(ip_text, ip_text_rect)
-        screen.blit(port_text, port_text_rect)
+        screen.blit(ip_password, ip_password_rect)
+        screen.blit(or_text, or_text_rect)
+        screen.blit(root_text, root_text_rect)
+        screen.blit(root_password, root_password_rect)
 
     # Display "Press menu button for exit" at the bottom of the screen
     exit_text = font.render("Press menu button for exit", True, (255, 255, 255))
     exit_text_rect = exit_text.get_rect(midbottom=(center_x, SCREEN_HEIGHT - 10))
     screen.blit(exit_text, exit_text_rect)
 
-def start_ftp_server():
-    start_ftp_command = "bftpd -c /mnt/SDCARD/Koriki/bin/bftpd.conf -D &"
-    SU.Popen(start_ftp_command, shell=True)
+def start_ssh_server():
+    start_ssh_command = "dropbear -R -B -b /mnt/SDCARD/App/ssh/banner.txt"
+    SU.Popen(start_ssh_command, shell=True)
 
-def stop_ftp_server():
-    stop_ftp_command = "killall bftpd"
-    SU.Popen(stop_ftp_command, shell=True)
+def stop_ssh_server():
+    with open('/var/run/dropbear.pid', 'r') as f:
+        dropbear_pid = f.read().strip()
+        
+    stop_ssh_command = "kill " + dropbear_pid
+    SU.Popen(stop_ssh_command, shell=True)
 
 def main():
     screen = initialize_pygame()
     font = pygame.font.SysFont(None, 30)
 
-    start_ftp_server()
-	
+    start_ssh_server()
+    
     colors = calculate_color(start_color, end_color, duration, steps)
     color_index = 0
 
@@ -97,11 +109,11 @@ def main():
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                stop_ftp_server()
+                stop_ssh_server()
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    stop_ftp_server()
+                    stop_ssh_server()
                     running = False
                     
         screen.fill(colors[color_index])
