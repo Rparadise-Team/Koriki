de4243a6a82f83118171a2dd4888afd838a58a6b
Ninoh-FOX <ninohfox@gmail.com>
2024-03-13 09:00:00 +0100

    rebuild libs EGL and GLESv2

--- base/App/OpenBOR/launch.sh
+++ base/App/OpenBOR/launch.sh
@@ -80,6 +80,7 @@ export PATH=$HOME:$PATH
 export LD_LIBRARY_PATH=$HOME/libs:$LD_LIBRARY_PATH
 export SDL_VIDEODRIVER=mmiyoo
 export SDL_AUDIODRIVER=mmiyoo
+export EGL_VIDEODRIVER=mmiyoo
 
 cd $HOME
 
