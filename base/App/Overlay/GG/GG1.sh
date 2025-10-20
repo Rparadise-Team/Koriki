58e0e77bf1c89a9b17b2b434911b4cd06c4cb9c2
Ninoh-FOX <ninohfox@gmail.com>
2025-10-20 13:37:49 +0200

    added provitional grip filte swithcher for v4/flip

--- base/App/Overlay/GG/GG1.sh
+++ base/App/Overlay/GG/GG1.sh
@@ -4,3 +4,14 @@ cd $(dirname "$0")
 HOME=/mnt/SDCARD
 ./OverlaySelector-GG-Gearsystem
 sync
+
+if dmesg | fgrep -q "FB_WIDTH=752"; then
+    find /mnt/SDCARD/RetroArch/.retroarch/config/ -type f -name "*.cfg" | while read file; do
+        if grep -q "input_overlay = \":/.retroarch/overlay/nothing.cfg\"" "$file"; then
+            if grep -q "video_filter = \":/.retroarch/filters/video/Grid3x.filt\"" "$file"; then
+                sed -i 's|video_filter = ":/.retroarch/filters/video/Grid3x.filt"|video_filter = ":/.retroarch/filters/video/Grid2x.filt"|g' "$file"
+            fi
+        fi
+    done
+    sync
+fi
