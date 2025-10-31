c5a9aa7fe6f63fc8032b53bbca40f577c4cd4083
Ninoh-FOX <ninohfox@gmail.com>
2025-10-31 11:38:48 +0100

    Update version format in release script

--- generate_release.sh
+++ generate_release.sh
@@ -3,9 +3,9 @@
 version=$1
 
 if [ ! ${1} ]; then
-echo "enter number version, example v1.0"
+echo "enter number version, example 1.0"
 else
 cd base
 echo ${1} > Koriki/version.txt
-zip -9 -x '*.gitignore' -x '*.git*' -r ../koriki_${1}_full.zip .
+zip -9 -x '*.gitignore' -x '*.git*' -r ../koriki_v${1}_full.zip .
 fi
